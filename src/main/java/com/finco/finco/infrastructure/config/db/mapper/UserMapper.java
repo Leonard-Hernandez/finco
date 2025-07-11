eff17d481dc7b0d5572721dabd109dba6868bb3e
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2025-07-10 23:06:39 -0500

    format

--- src/main/java/com/finco/finco/infrastructure/config/db/mapper/UserMapper.java
+++ src/main/java/com/finco/finco/infrastructure/config/db/mapper/UserMapper.java
@@ -26,170 +26,173 @@ import com.finco.finco.infrastructure.config.db.schema.UserSchema;
 @Component
 public class UserMapper {
 
-    private final AccountMapper accountMapper;
-    private final GoalMapper goalMapper;
-    private final AssetMapper assetMapper;
-    private final LiabilitieMapper liabilitieMapper;
-    private final RoleMapper roleMapper;
-    private final TransactionMapper transactionMapper;
+        private final AccountMapper accountMapper;
+        private final GoalMapper goalMapper;
+        private final AssetMapper assetMapper;
+        private final LiabilitieMapper liabilitieMapper;
+        private final RoleMapper roleMapper;
+        private final TransactionMapper transactionMapper;
 
-    public UserMapper(AccountMapper accountMapper, GoalMapper GoalMapper, AssetMapper assetMapper,
-            LiabilitieMapper liabilitieMapper, RoleMapper roleMapper, TransactionMapper transactionMapper) {
-        this.accountMapper = accountMapper;
-        this.goalMapper = GoalMapper;
-        this.assetMapper = assetMapper;
-        this.liabilitieMapper = liabilitieMapper;
-        this.roleMapper = roleMapper;
-        this.transactionMapper = transactionMapper;
-    }
-
-    public User toUser(UserSchema userSchema) {
-        if (userSchema == null) {
-            return null;
+        public UserMapper(AccountMapper accountMapper, GoalMapper GoalMapper, AssetMapper assetMapper,
+                        LiabilitieMapper liabilitieMapper, RoleMapper roleMapper, TransactionMapper transactionMapper) {
+                this.accountMapper = accountMapper;
+                this.goalMapper = GoalMapper;
+                this.assetMapper = assetMapper;
+                this.liabilitieMapper = liabilitieMapper;
+                this.roleMapper = roleMapper;
+                this.transactionMapper = transactionMapper;
         }
 
-        User user = new User();
+        public User toUser(UserSchema userSchema) {
+                if (userSchema == null) {
+                        return null;
+                }
 
-        user.setId(userSchema.getId());
-        user.setName(userSchema.getName());
-        user.setEmail(userSchema.getEmail());
-        user.setPassword(userSchema.getPassword());
-        user.setRegistrationDate(userSchema.getRegistrationDate());
-        user.setEnable(userSchema.getEnable());
+                User user = new User();
 
-        List<Account> accounts = userSchema.getAccounts() != null
-                ? userSchema.getAccounts().stream().map(accountMapper::toAccount).collect(Collectors.toList())
-                : List.of();
+                user.setId(userSchema.getId());
+                user.setName(userSchema.getName());
+                user.setEmail(userSchema.getEmail());
+                user.setPassword(userSchema.getPassword());
+                user.setRegistrationDate(userSchema.getRegistrationDate());
+                user.setEnable(userSchema.getEnable());
 
-        List<Goal> goals = userSchema.getGoals() != null
-                ? userSchema.getGoals().stream().map(goalMapper::toGoal).collect(Collectors.toList())
-                : List.of();
+                List<Account> accounts = userSchema.getAccounts() != null
+                                ? userSchema.getAccounts().stream().map(accountMapper::toAccount)
+                                                .collect(Collectors.toList())
+                                : List.of();
 
-        List<Asset> assets = userSchema.getAssets() != null
-                ? userSchema.getAssets().stream().map(assetMapper::toAsset).collect(Collectors.toList())
-                : List.of();
+                List<Goal> goals = userSchema.getGoals() != null
+                                ? userSchema.getGoals().stream().map(goalMapper::toGoal).collect(Collectors.toList())
+                                : List.of();
 
-        List<Liabilitie> liabilities = userSchema.getLiabilities() != null
-                ? userSchema.getLiabilities().stream().map(liabilitieMapper::toLiabilitie).collect(Collectors.toList())
-                : List.of();
+                List<Asset> assets = userSchema.getAssets() != null
+                                ? userSchema.getAssets().stream().map(assetMapper::toAsset).collect(Collectors.toList())
+                                : List.of();
 
-        List<Role> roles = userSchema.getRoles() != null
-                ? userSchema.getRoles().stream().map(roleMapper::toRole).collect(Collectors.toList())
-                : List.of();
+                List<Liabilitie> liabilities = userSchema.getLiabilities() != null
+                                ? userSchema.getLiabilities().stream().map(liabilitieMapper::toLiabilitie)
+                                                .collect(Collectors.toList())
+                                : List.of();
 
-        List<Transaction> transactions = userSchema.getTransactions() != null
-                ? userSchema.getTransactions().stream().map(transactionMapper::toTransaction)
-                        .collect(Collectors.toList())
-                : List.of();
+                List<Role> roles = userSchema.getRoles() != null
+                                ? userSchema.getRoles().stream().map(roleMapper::toRole).collect(Collectors.toList())
+                                : List.of();
 
-        user.setAccounts(accounts);
-        user.setGoals(goals);
-        user.setAssets(assets);
-        user.setLiabilities(liabilities);
-        user.setRoles(roles);
-        user.setTransactions(transactions);
+                List<Transaction> transactions = userSchema.getTransactions() != null
+                                ? userSchema.getTransactions().stream().map(transactionMapper::toTransaction)
+                                                .collect(Collectors.toList())
+                                : List.of();
 
-        return user;
+                user.setAccounts(accounts);
+                user.setGoals(goals);
+                user.setAssets(assets);
+                user.setLiabilities(liabilities);
+                user.setRoles(roles);
+                user.setTransactions(transactions);
 
-    }
+                return user;
 
-    public User toLigthUser(UserSchema userSchema) {
-        if (userSchema == null) {
-            return null;
         }
 
-        User user = new User();
+        public User toLigthUser(UserSchema userSchema) {
+                if (userSchema == null) {
+                        return null;
+                }
 
-        user.setId(userSchema.getId());
-        user.setName(userSchema.getName());
-        user.setEmail(userSchema.getEmail());
-        user.setPassword(userSchema.getPassword());
-        user.setRegistrationDate(userSchema.getRegistrationDate());
-        user.setEnable(userSchema.getEnable());
+                User user = new User();
 
-        return user;
-    }
+                user.setId(userSchema.getId());
+                user.setName(userSchema.getName());
+                user.setEmail(userSchema.getEmail());
+                user.setPassword(userSchema.getPassword());
+                user.setRegistrationDate(userSchema.getRegistrationDate());
+                user.setEnable(userSchema.getEnable());
 
-    public UserSchema toUserSchema(User user) {
-        if (user == null) {
-            return null;
+                return user;
         }
 
-        UserSchema userSchema = new UserSchema();
+        public UserSchema toUserSchema(User user) {
+                if (user == null) {
+                        return null;
+                }
 
-        userSchema.setId(user.getId());
-        userSchema.setName(user.getName());
-        userSchema.setEmail(user.getEmail());
-        userSchema.setPassword(user.getPassword());
-        userSchema.setRegistrationDate(user.getRegistrationDate());
-        userSchema.setEnable(user.isEnable());
+                UserSchema userSchema = new UserSchema();
 
-        // Create minimal account schemas with just the IDs to prevent circular references
-        List<AccountSchema> accounts = user.getAccounts() != null
-                ? user.getAccounts().stream().map(account -> {
-                    AccountSchema accountSchema = new AccountSchema();
-                    accountSchema.setId(account.getId());
-                    accountSchema.setName(account.getName());
-                    accountSchema.setType(account.getType());
-                    accountSchema.setBalance(account.getBalance());
-                    accountSchema.setCurrency(account.getCurrency());
-                    accountSchema.setDefault(account.isDefault());
-                    accountSchema.setEnable(account.isEnable());
-                    return accountSchema;
-                }).collect(Collectors.toList())
-                : List.of();
+                userSchema.setId(user.getId());
+                userSchema.setName(user.getName());
+                userSchema.setEmail(user.getEmail());
+                userSchema.setPassword(user.getPassword());
+                userSchema.setRegistrationDate(user.getRegistrationDate());
+                userSchema.setEnable(user.isEnable());
 
-        List<GoalSchema> goals = user.getGoals() != null
-                ? user.getGoals().stream().map(goalMapper::toGoalSchema).collect(Collectors.toList())
-                : List.of();
+                // Create minimal account schemas with just the IDs to prevent circular
+                // references
+                List<AccountSchema> accounts = user.getAccounts() != null
+                                ? user.getAccounts().stream().map(account -> {
+                                        AccountSchema accountSchema = new AccountSchema();
+                                        accountSchema.setId(account.getId());
+                                        accountSchema.setName(account.getName());
+                                        accountSchema.setType(account.getType());
+                                        accountSchema.setBalance(account.getBalance());
+                                        accountSchema.setCurrency(account.getCurrency());
+                                        accountSchema.setDefault(account.isDefault());
+                                        accountSchema.setEnable(account.isEnable());
+                                        return accountSchema;
+                                }).collect(Collectors.toList())
+                                : List.of();
 
-        List<AssetSchema> assets = user.getAssets() != null
-                ? user.getAssets().stream().map(assetMapper::toAssetSchema).collect(Collectors.toList())
-                : List.of();
+                List<GoalSchema> goals = user.getGoals() != null
+                                ? user.getGoals().stream().map(goalMapper::toGoalSchema).collect(Collectors.toList())
+                                : List.of();
 
-        List<LiabilitieSchema> liabilities = user.getLiabilities() != null
-                ? user.getLiabilities().stream().map(liabilitieMapper::toLiabilitieSchema).collect(Collectors.toList())
-                : List.of();
+                List<AssetSchema> assets = user.getAssets() != null
+                                ? user.getAssets().stream().map(assetMapper::toAssetSchema).collect(Collectors.toList())
+                                : List.of();
 
-        List<RoleSchema> roles = user.getRoles() != null
-                ? user.getRoles().stream().map(roleMapper::toRoleSchema).collect(Collectors.toList())
-                : List.of();
+                List<LiabilitieSchema> liabilities = user.getLiabilities() != null
+                                ? user.getLiabilities().stream().map(liabilitieMapper::toLiabilitieSchema)
+                                                .collect(Collectors.toList())
+                                : List.of();
 
-        List<TransactionSchema> transactions = user.getTransactions() != null
-                ? user.getTransactions().stream().map(transactionMapper::toTransactionSchema)
-                        .collect(Collectors.toList())
-                : List.of();
+                List<RoleSchema> roles = user.getRoles() != null
+                                ? user.getRoles().stream().map(roleMapper::toRoleSchema).collect(Collectors.toList())
+                                : List.of();
 
-        userSchema.setAccounts(accounts);
-        userSchema.setGoals(goals);
-        userSchema.setAssets(assets);
-        userSchema.setLiabilities(liabilities);
-        userSchema.setRoles(roles);
-        userSchema.setTransactions(transactions);
+                List<TransactionSchema> transactions = user.getTransactions() != null
+                                ? user.getTransactions().stream().map(transactionMapper::toTransactionSchema)
+                                                .collect(Collectors.toList())
+                                : List.of();
 
-        return userSchema;
-    }
+                userSchema.setAccounts(accounts);
+                userSchema.setGoals(goals);
+                userSchema.setAssets(assets);
+                userSchema.setLiabilities(liabilities);
+                userSchema.setRoles(roles);
+                userSchema.setTransactions(transactions);
 
-    public PagedResult<User> toUserPagedResult(Page<UserSchema> userSchemaPage, PageRequest pageRequest) {
-        if (userSchemaPage == null) {
-            return PagedResult.empty(pageRequest);
+                return userSchema;
         }
 
-        List<User> userList = userSchemaPage.getContent().stream()
-                                            .map(this::toLigthUser)
-                                            .collect(Collectors.toList());
+        public PagedResult<User> toUserPagedResult(Page<UserSchema> userSchemaPage, PageRequest pageRequest) {
+                if (userSchemaPage == null) {
+                        return PagedResult.empty(pageRequest);
+                }
 
-        return new PagedResult<>(
-            userList,
-            userSchemaPage.getTotalElements(),
-            userSchemaPage.getTotalPages(),
-            userSchemaPage.getNumber(),
-            userSchemaPage.getSize(),
-            userSchemaPage.isFirst(),
-            userSchemaPage.isLast(),
-            userSchemaPage.hasNext(),
-            userSchemaPage.hasPrevious()
-        );
-    }
+                List<User> userList = userSchemaPage.getContent().stream()
+                                .map(this::toLigthUser)
+                                .collect(Collectors.toList());
+
+                return new PagedResult<>(
+                                userList,
+                                userSchemaPage.getTotalElements(),
+                                userSchemaPage.getTotalPages(),
+                                userSchemaPage.getNumber(),
+                                userSchemaPage.getSize(),
+                                userSchemaPage.isFirst(),
+                                userSchemaPage.isLast(),
+                                userSchemaPage.hasNext(),
+                                userSchemaPage.hasPrevious());
+        }
 
 }
