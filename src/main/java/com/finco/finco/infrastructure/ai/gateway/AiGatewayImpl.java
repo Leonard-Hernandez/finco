da1ee12cbf90dd4c20244b2c5dd0dbf8adae5329
Leonard <Leonardhernandez02@gmail.com>
2025-12-09 23:49:46 -0500

    working in chat memory

--- src/main/java/com/finco/finco/infrastructure/ai/gateway/AiGatewayImpl.java
+++ src/main/java/com/finco/finco/infrastructure/ai/gateway/AiGatewayImpl.java
@@ -1,6 +1,9 @@
 package com.finco.finco.infrastructure.ai.gateway;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.stereotype.Service;
 
 import com.finco.finco.entity.ai.gateway.AiGateway;
@@ -11,19 +14,26 @@ import com.finco.finco.infrastructure.transaction.gateway.TransactionAiTools;
 public class AiGatewayImpl implements AiGateway {
 
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
 
     public AiGatewayImpl(ChatClient.Builder builder, TransactionAiTools transactionAiTools,
             AccountAiTools accountAiTools) {
 
-        this.chatClient = builder.defaultTools(transactionAiTools, accountAiTools).build();
+        this.chatMemory = MessageWindowChatMemory.builder().build();
+        this.chatClient = builder.defaultTools(transactionAiTools, accountAiTools)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @Override
     public String getAnswer(String question, Long userId) {
 
-        return chatClient.prompt().user(question + " datos: id: " + userId).system(
-                "You are finco a finacial expert and you rol is help the user with their personal finance using the tools").call().content();
-
+        return chatClient.prompt()
+                .user(question + " datos: id: " + userId)
+                .system(
+                        "You are finco a finacial expert and you rol is help the user with their personal finance using the tools")
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, userId))
+                .call().content();
 
     }
 
