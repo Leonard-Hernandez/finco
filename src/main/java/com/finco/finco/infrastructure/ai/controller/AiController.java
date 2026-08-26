2afea81a73dcf27c11b9fbbec6492e16b56adae8
leonardh <leonardhernandez02@gmail.com>
2026-08-26 18:06:34 -0500

    chore: improve ai controller

--- src/main/java/com/finco/finco/infrastructure/ai/controller/AiController.java
+++ src/main/java/com/finco/finco/infrastructure/ai/controller/AiController.java
@@ -1,18 +1,15 @@
 package com.finco.finco.infrastructure.ai.controller;
 
-import java.security.Principal;
 import java.time.LocalDateTime;
 
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import com.finco.finco.infrastructure.ai.dto.AiAskDto;
 import com.finco.finco.infrastructure.config.error.ErrorResponse;
-import com.finco.finco.infrastructure.config.security.services.WebSocketSessionHolder;
 import com.finco.finco.usecase.ai.AiGetAnswerUseCase;
 
 @Controller
@@ -26,14 +23,9 @@ public class AiController {
 
     @MessageMapping(value = "/chat")
     @SendToUser("/queue/chat")
-    public String SendMessage(@Payload AiAskDto aiAskDto, Principal principal, SimpMessageHeaderAccessor headerAccessor)
+    public String SendMessage(@Payload AiAskDto aiAskDto)
             throws Exception {
-        try {
-            WebSocketSessionHolder.setSessionId(headerAccessor.getSessionId());
             return aiGetAnswerUseCase.execute(aiAskDto);
-        } finally {
-            WebSocketSessionHolder.clear();
-        }
     }
 
     @MessageExceptionHandler(Exception.class)
